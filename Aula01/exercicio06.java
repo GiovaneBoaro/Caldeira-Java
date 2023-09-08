d6bde8a9634ebd4a1b87eb76833802bcff33abab
GiovaneBoaro <giovane.boaro@gmail.com>
2023-09-08 16:33:52 -0300

    exercicio 6

---
+++ Aula01/exercicio06.java
@@ -0,0 +1,20 @@
+package Aula01;
+
+import java.util.Scanner;
+
+public class exercicio06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+
+        if (idade < 16) {
+            System.out.println("Você não pode votar!");
+        } else if (idade == 16 || idade == 17 || idade > 65) {
+            System.out.println("Seu voto é facultativo!");
+        } else {
+            System.out.println("Seu voto é obrigatório!");
+        }
+    }
+}
