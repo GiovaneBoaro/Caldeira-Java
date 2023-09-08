0e0458fcf63105b9dd1b55467403af6496c42be6
GiovaneBoaro <giovane.boaro@gmail.com>
2023-09-08 15:12:09 -0300

    exercicio 4

---
+++ Aula01/exercicio04.java
@@ -0,0 +1,18 @@
+package Aula01;
+
+import java.util.Scanner;
+
+public class exercicio04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double salarioMinimo = 1320;
+
+        System.out.println("Digite o valor do seu salário: ");
+        double salarioDigitado = scanner.nextDouble();
+
+        int quantidadeDeSalariosMinimos = (int) (salarioDigitado / salarioMinimo);
+
+        System.out.println("Você ganha " + quantidadeDeSalariosMinimos + " salário(s) mínimo(s).");
+
+    }
+}
