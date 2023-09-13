c1f85fc4513a07fa3f1868ec209230e06e833a6e
GiovaneBoaro <giovane.boaro@gmail.com>
2023-09-13 20:05:28 -0300

    exercicio 1 aula 2

---
+++ Aula02/exercicio01.java
@@ -0,0 +1,24 @@
+package Aula02;
+
+import java.util.Scanner;
+
+public class exercicio01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escreva um número: ");
+        int num = scanner.nextInt();
+
+        for(int cont1 = 2; cont1 <= num; cont1++) {
+            boolean numeroPrimo = true;
+            for(int cont2 = 2; cont2 < cont1; cont2++) {
+                if(cont1 % cont2 == 0) {
+                    numeroPrimo = false;
+                }
+            }
+            if(numeroPrimo) {
+                System.out.println(cont1);
+            }
+        }
+    }
+}
