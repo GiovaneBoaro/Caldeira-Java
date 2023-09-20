5e2537901ab9b19b15687c73b2005e5474ece165
GiovaneBoaro <giovane.boaro@gmail.com>
2023-09-20 16:48:29 -0300

    aula 3 exercicio 2: Circulo

---
+++ Aula03/CirculoConsole.java
@@ -0,0 +1,22 @@
+package Aula03;
+
+import java.util.Scanner;
+
+public class CirculoConsole {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Circulo circulo1 = new Circulo(1);
+        System.out.println("Informe o raio do circulo: ");
+        circulo1.setRaio(Double.parseDouble(scanner.nextLine()));
+        System.out.println(circulo1.toString());
+
+        Circulo circulo2 = new Circulo(2);
+        System.out.println("Informe o raio do circulo: ");
+        circulo2.setRaio(Double.parseDouble(scanner.nextLine()));
+        System.out.println(circulo2.toString());
+
+
+        System.out.println(circulo1.equals(circulo2));
+    }
+}
