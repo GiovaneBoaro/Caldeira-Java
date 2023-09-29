baaff1d61d956841f25e3705e027a4eaeca10b75
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2023-09-29 10:58:46 -0300

    Arrumando o nome dos packages e classes do repositório.

--- aula05/Exercicio03.java
+++ aula05/Exercicio03.java
@@ -11,13 +11,12 @@ public class Exercicio03 {
 
         while(opcao != 0) {
             System.out.println("***************************");
-            System.out.println("Programa para lista de tarefas");
+            System.out.println("Programa lista de tarefas");
             System.out.println("Digite o que você quer fazer: ");
             System.out.println("0 - Sair");
-            System.out.println("1 - Definir nome para a lista");
-            System.out.println("2 - Adicionar tarefa");
-            System.out.println("3 - Remover tarefa");
-            System.out.println("4 - Ver todas as tarefas");
+            System.out.println("1 - Adicionar tarefa");
+            System.out.println("2 - Remover tarefa");
+            System.out.println("3 - Ver todas as tarefas");
             opcao = Integer.parseInt(scanner.nextLine());
 
             int i;
@@ -27,27 +26,31 @@ public class Exercicio03 {
                     System.out.println("Obrigado por utilizar o programa!");
                     break;
                 case 1:
-                    System.out.println("Digite o nome da lista: ");
-                    String nomeLista = scanner.nextLine();
-                    System.out.println("Lista " + nomeLista + " definido(a) com sucesso!");
+                    System.out.println("Titulo da tarefa: ");
+                    String titulo = scanner.nextLine();
+
+                    System.out.println("Descrição da tarefa: ");
+                    String descricao = scanner.nextLine();
+
+                    System.out.println("Data da tarefa: ");
+                    String data = scanner.nextLine();
+
+                    String tarefa = titulo + " - " + descricao + " - " + data;
+
+                    lista.add(tarefa);
+                    System.out.println(" Tarefa adicionada");
                     break;
                 case 2:
-                    System.out.println("Qual a tarefa?");
-                    String tarefa = scanner.nextLine();
-                    lista.add(tarefa);
-                    System.out.println(tarefa + " adicionado(a)");
-                    break;
-                case 3:
-                    System.out.println("Qual tarefa você deseja excluir?");
+                    System.out.println("Qual tarefa você deseja excluir? Escolha pelo índice");
                     i = Integer.parseInt(scanner.nextLine());
 
                     try {
-                        lista.remove(i);
+                        lista.remove(i -1);
                     } catch (IndexOutOfBoundsException e) {
                         System.out.println("Tarefa inválida!");
                     }
                     break;
-                case 4:
+                case 3:
                     System.out.println("Todas as tarefas criadas: ");
                     int n = lista.size();
                     for(i = 0; i < n; i++) {
